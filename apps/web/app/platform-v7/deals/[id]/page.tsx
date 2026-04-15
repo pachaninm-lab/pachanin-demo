70cb17bad554e7419e662065fc5b5e66e50dc1ff
pachaninm-lab <pachaninm@gmail.com>
2026-04-15 19:29:32 +0300

    update

--- apps/web/app/platform-v7/deals/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DealReadinessPage } from '@/components/v7r/EsiaFgisRuntime';
-
-export default function PlatformV7DealReadinessPage({ params }: { params: { id: string } }) {
-  return <DealReadinessPage id={params.id} />;
-}
