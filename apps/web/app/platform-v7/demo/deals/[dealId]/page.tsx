934ac8d3db4f8e5eaf65e9c4171be5784e6557c8
pachaninm-lab <pachaninm@gmail.com>
2026-04-18 15:06:44 +0300

    fix(v7): remove conflicting dynamic demo route

--- apps/web/app/platform-v7/demo/deals/[dealId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LiveDealInvestorRuntime } from '@/components/v7r/LiveDealInvestorRuntime';
-
-export default function DemoDealPage({ params }) {
-  return <LiveDealInvestorRuntime id={params.dealId} />;
-}
