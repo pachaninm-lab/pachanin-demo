83e1f204b1d3a5094e9b013d49996fd853b0e1a2
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 11:57:02 +0300

    Add platform v7 deal SDIZ route

---
+++ apps/web/app/platform-v7/deals/grain-sdiz/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7DealSdizPage() {
+  return <GrainExecutionPage mode='deal-sdiz' role='operator' />;
+}
