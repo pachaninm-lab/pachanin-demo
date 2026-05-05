8028928da1b3033d3ed0698fc8e3c46ff7012d82
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 11:39:32 +0300

    Add platform v7 seller quick sale route

---
+++ apps/web/app/platform-v7/seller/quick-sale/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7SellerQuickSalePage() {
+  return <GrainExecutionPage mode='quick-sale' role='seller' />;
+}
