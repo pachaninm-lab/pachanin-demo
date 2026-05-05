1a7214ea26d7ebc03c109e2794741a84bd31578b
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 11:36:17 +0300

    Add platform v7 grain batches route

---
+++ apps/web/app/platform-v7/batches/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7BatchesPage() {
+  return <GrainExecutionPage mode='batches' role='seller' />;
+}
