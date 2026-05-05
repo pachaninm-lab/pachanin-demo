7d695447a82a9cc5c40dc0240c5db41fc9fdf607
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 12:17:34 +0300

    Add grain batch detail route

---
+++ apps/web/app/platform-v7/batches/[batchId]/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7BatchByIdPage() {
+  return <GrainExecutionPage mode='batch-detail' role='seller' />;
+}
