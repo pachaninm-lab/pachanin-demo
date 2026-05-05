2fa4760b0abf1cac3369033d0a22faf0d9114cf3
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 11:57:38 +0300

    Add platform v7 deal money release route

---
+++ apps/web/app/platform-v7/deals/grain-release/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7DealReleasePage() {
+  return <GrainExecutionPage mode='deal-release' role='bank' />;
+}
