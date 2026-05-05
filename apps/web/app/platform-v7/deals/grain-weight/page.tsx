0e1cf6a3aae8e1a6ef62c5e6f4936448570a4321
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 11:56:03 +0300

    Add platform v7 deal weight route

---
+++ apps/web/app/platform-v7/deals/grain-weight/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7DealWeightPage() {
+  return <GrainExecutionPage mode='deal-weight' role='operator' />;
+}
