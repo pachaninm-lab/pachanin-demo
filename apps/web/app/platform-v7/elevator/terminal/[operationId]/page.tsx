01bee15e8152bc0e4af31d20ff5f03406027bd41
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 12:20:05 +0300

    Add elevator operation route

---
+++ apps/web/app/platform-v7/elevator/terminal/[operationId]/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7ElevatorOperationPage() {
+  return <GrainExecutionPage mode='elevator-operation' role='elevator' />;
+}
