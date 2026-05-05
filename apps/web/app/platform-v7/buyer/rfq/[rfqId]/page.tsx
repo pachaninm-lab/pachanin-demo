a505b7deca10f06a5558610deabd986ec276170f
pachaninm-lab <pachaninm@gmail.com>
2026-05-05 12:14:26 +0300

    Add dynamic RFQ grain execution route

---
+++ apps/web/app/platform-v7/buyer/rfq/[rfqId]/page.tsx
@@ -0,0 +1,5 @@
+import { GrainExecutionPage } from '@/components/platform-v7/GrainExecutionPage';
+
+export default function PlatformV7RfqByIdPage() {
+  return <GrainExecutionPage mode='rfq-detail' role='buyer' />;
+}
