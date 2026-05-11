f43793002fc1b94202b0ae224cde8ac356e0f8be
pachaninm-lab <pachaninm@gmail.com>
2026-05-11 23:34:54 +0300

    feat(platform-v7): add buyer guarded decision pack placement

---
+++ apps/web/components/platform-v7/BuyerDecisionPackRoutePlacement.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { DecisionPackRoutePlacement } from './DecisionPackRoutePlacement';
+
+export function BuyerDecisionPackRoutePlacement() {
+  return <DecisionPackRoutePlacement route='/platform-v7/buyer' context='buyer_reserve_request' />;
+}
