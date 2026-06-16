21b3a99f82c639053cb5ef7de7973b1f664f02d6
pachaninm-lab <pachaninm@gmail.com>
2026-06-17 02:26:41 +0300

    chore(platform-v7): add guard imports

--- apps/web/components/platform-v7/PlatformV7SingleEntryGuard.tsx
+++ apps/web/components/platform-v7/PlatformV7SingleEntryGuard.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import * as React from 'react';
+import { usePathname } from 'next/navigation';
+
 export function PlatformV7SingleEntryGuard() {
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    void pathname;
+  }, [pathname]);
+
   return null;
 }
