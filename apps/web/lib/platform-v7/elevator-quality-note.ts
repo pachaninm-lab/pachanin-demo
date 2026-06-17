89d5b70b59dad790c8015e086853e334c71f8fa0
pachaninm-lab <pachaninm@gmail.com>
2026-06-18 00:11:13 +0300

    fix(platform-v7): type elevator quality note field

---
+++ apps/web/lib/platform-v7/elevator-quality-note.ts
@@ -0,0 +1,9 @@
+import './deal-execution-source-of-truth';
+
+declare module './deal-execution-source-of-truth' {
+  interface ElevatorQualityMetric {
+    readonly note?: string;
+  }
+}
+
+export {};
