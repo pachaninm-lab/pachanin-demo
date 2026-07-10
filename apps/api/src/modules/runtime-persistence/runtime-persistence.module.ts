d698d1755218bcdecc67a88ca7db7230248c7948
pachaninm-lab <pachaninm@gmail.com>
2026-07-10 12:23:19 +0300

    Resolve VP-3.42 runtime command service integration

--- apps/api/src/modules/runtime-persistence/runtime-persistence.module.ts
+++ apps/api/src/modules/runtime-persistence/runtime-persistence.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { PrismaService } from '../../common/prisma/prisma.service';
+import { RuntimePersistenceCommandService } from './runtime-persistence-command.service';
 import { selectRuntimePersistenceRepository } from './runtime-persistence-repository.factory';
 import { RUNTIME_PERSISTENCE_REPOSITORY } from './runtime-persistence.repository';
 import { RuntimePersistenceService } from './runtime-persistence.service';
@@ -9,12 +10,17 @@ import { RuntimePersistenceService } from './runtime-persistence.service';
   imports: [PrismaModule],
   providers: [
     RuntimePersistenceService,
+    RuntimePersistenceCommandService,
     {
       provide: RUNTIME_PERSISTENCE_REPOSITORY,
       inject: [PrismaService],
       useFactory: (prisma: PrismaService) => selectRuntimePersistenceRepository(prisma),
     },
   ],
-  exports: [RuntimePersistenceService, RUNTIME_PERSISTENCE_REPOSITORY],
+  exports: [
+    RuntimePersistenceService,
+    RuntimePersistenceCommandService,
+    RUNTIME_PERSISTENCE_REPOSITORY,
+  ],
 })
 export class RuntimePersistenceModule {}
