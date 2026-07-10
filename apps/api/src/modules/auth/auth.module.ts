eef4c643b348ab89fcb63b17c3ce42fed9736377
pachaninm-lab <pachaninm@gmail.com>
2026-07-10 15:54:37 +0300

    Switch auth token to industrial PostgreSQL service

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { BusinessReputationModule } from '../business-reputation/business-reputation.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { IndustrialAuthService } from './industrial-auth.service';
 import { PersistentAuthSessionService } from './persistent-auth-session.service';
 
 @Module({
   imports: [BusinessReputationModule],
   controllers: [AuthController],
-  providers: [AuthService, PersistentAuthSessionService],
+  providers: [
+    PersistentAuthSessionService,
+    IndustrialAuthService,
+    { provide: AuthService, useExisting: IndustrialAuthService },
+  ],
   exports: [AuthService, PersistentAuthSessionService],
 })
 export class AuthModule {}
