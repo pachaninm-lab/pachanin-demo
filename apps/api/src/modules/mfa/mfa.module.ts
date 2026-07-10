c5e2ee22833dbfdd6ba1248168fb05c7e1137d0c
pachaninm-lab <pachaninm@gmail.com>
2026-07-10 20:33:11 +0300

    revert(mfa): keep stateless helper module independent

--- apps/api/src/modules/mfa/mfa.module.ts
+++ apps/api/src/modules/mfa/mfa.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
-import { MfaController } from './mfa.controller';
 import { MfaService } from './mfa.service';
+import { MfaController } from './mfa.controller';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [MfaController],
   providers: [MfaService],
+  controllers: [MfaController],
   exports: [MfaService],
 })
 export class MfaModule {}
