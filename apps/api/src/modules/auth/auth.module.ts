746a7dd11a577c17e7ea54dd1fcc085ed8ef771e
pachaninm-lab <pachaninm@gmail.com>
2026-07-10 18:16:32 +0300

    Register persistent auth repository

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BusinessReputationModule } from '../business-reputation/business-reputation.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PersistentAuthRepository } from './persistent-auth.repository';
 
 @Module({
   imports: [BusinessReputationModule],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService]
+  providers: [PersistentAuthRepository, AuthService],
+  exports: [AuthService, PersistentAuthRepository],
 })
 export class AuthModule {}
