066d7ce4c4bf0f8bbe9ea751c9dca6eb94a0a1b4
pachaninm-lab <pachaninm@gmail.com>
2026-07-10 21:48:06 +0300

    refactor(arbitrator): remove process-local ledger provider

--- apps/api/src/modules/arbitrator/arbitrator.module.ts
+++ apps/api/src/modules/arbitrator/arbitrator.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArbitratorService } from './arbitrator.service';
 import { ArbitratorController } from './arbitrator.controller';
-import { AuditModule } from '../audit/audit.module';
-import { LedgerV2Service } from '../ledger/ledger-v2.service';
-import { ActionExecutorModule } from '../../common/action-executor/action-executor.module';
+import { DisputesModule } from '../disputes/disputes.module';
 
 @Module({
-  imports: [AuditModule, ActionExecutorModule],
-  providers: [ArbitratorService, LedgerV2Service],
+  imports: [DisputesModule],
+  providers: [ArbitratorService],
   controllers: [ArbitratorController],
   exports: [ArbitratorService],
 })
