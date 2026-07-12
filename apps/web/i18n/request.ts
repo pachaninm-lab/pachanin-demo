3a8a42741749491ecbcb20ec17c3e5a4cb6bebc7
pachaninm-lab <pachaninm@gmail.com>
2026-07-12 13:57:26 +0300

    Fix Russian as default locale on clean entry

--- apps/web/i18n/request.ts
+++ apps/web/i18n/request.ts
@@ -1,33 +1,19 @@
-import { cookies, headers } from 'next/headers';
+import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
-import { DEFAULT_LOCALE, LOCALE_COOKIE, isAppLocale } from './locale';
+import { DEFAULT_LOCALE, isAppLocale } from './locale';
 import { publicEntryMessages } from './public-entry-messages';
 
 const LOCALE_HEADER = 'x-pc-locale';
 
 export default getRequestConfig(async () => {
   let locale = DEFAULT_LOCALE;
-  let headerLocaleResolved = false;
 
   try {
     const headerStore = await headers();
     const headerLocale = headerStore.get(LOCALE_HEADER);
-    if (isAppLocale(headerLocale)) {
-      locale = headerLocale;
-      headerLocaleResolved = true;
-    }
+    if (isAppLocale(headerLocale)) locale = headerLocale;
   } catch {
     locale = DEFAULT_LOCALE;
-  }
-
-  if (!headerLocaleResolved) {
-    try {
-      const cookieStore = await cookies();
-      const cookieLocale = cookieStore.get(LOCALE_COOKIE)?.value;
-      if (isAppLocale(cookieLocale)) locale = cookieLocale;
-    } catch {
-      locale = DEFAULT_LOCALE;
-    }
   }
 
   const baseMessages = (await import(`../messages/${locale}.json`)).default;
