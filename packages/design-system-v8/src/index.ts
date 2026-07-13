881f3c78127929b9177ba3210c283ae2604c8e40
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 19:34:59 +0300

    Export design system v8 components

---
+++ packages/design-system-v8/src/index.ts
@@ -0,0 +1,15 @@
+export {
+  Button,
+  InlineNotice,
+  NextActionCard,
+  StatusChip,
+  Surface,
+} from './components';
+
+export type {
+  ButtonProps,
+  InlineNoticeProps,
+  NextActionCardProps,
+  StatusChipProps,
+  SurfaceProps,
+} from './components';
