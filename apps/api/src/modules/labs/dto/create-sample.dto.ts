10929df1ecfe63877c48b3c3d8dfdf65638818e7
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 10:27:59 +0300

    feat(labs): require durable create command metadata

--- apps/api/src/modules/labs/dto/create-sample.dto.ts
+++ apps/api/src/modules/labs/dto/create-sample.dto.ts
@@ -1,14 +1,41 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsISO8601, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateSampleDto {
   @IsString()
+  @MinLength(1)
+  @MaxLength(160)
   dealId!: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(160)
   shipmentId?: string;
 
   @IsOptional()
   @IsString()
-  note?: string;
+  @MaxLength(160)
+  acceptanceId?: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(200)
+  evidenceRef!: string;
+
+  @IsISO8601()
+  occurredAt!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(200)
+  commandId!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(200)
+  idempotencyKey!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  correlationId?: string;
 }
