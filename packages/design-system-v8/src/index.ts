e55508456d7ba6ac6c0b7c55a591153b44a9397d
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 19:54:36 +0300

    Export transaction templates from Design System v8

--- packages/design-system-v8/src/index.ts
+++ packages/design-system-v8/src/index.ts
@@ -1,15 +1,25 @@
 export {
+  AiTransparency,
+  AppFrame,
   Button,
   InlineNotice,
+  KeyFact,
+  KeyFactGrid,
   NextActionCard,
   StatusChip,
   Surface,
+  WorkbenchTemplate,
 } from './components';
 
 export type {
+  AiTransparencyProps,
+  AppFrameProps,
   ButtonProps,
   InlineNoticeProps,
+  KeyFactProps,
   NextActionCardProps,
   StatusChipProps,
+  StatusTone,
   SurfaceProps,
+  WorkbenchTemplateProps,
 } from './components';
