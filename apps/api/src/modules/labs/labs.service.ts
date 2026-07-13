018df237fd7900529a1f5baf1920a461e331b590
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 10:53:36 +0300

    refactor(labs): propagate trusted user and typed commands

--- apps/api/src/modules/labs/labs.service.ts
+++ apps/api/src/modules/labs/labs.service.ts
@@ -1,5 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
+import type { RequestUser } from '../../common/types/request-user';
+import { CollectSampleDto } from './dto/collect-sample.dto';
 import { CreateSampleDto } from './dto/create-sample.dto';
+import { FinalizeSampleDto } from './dto/finalize-sample.dto';
 import { RecordTestDto } from './dto/record-test.dto';
 import { LAB_REPOSITORY, type LabRepository } from './lab.repository';
 
@@ -9,27 +12,27 @@ export class LabsService {
     @Inject(LAB_REPOSITORY) private readonly labs: LabRepository,
   ) {}
 
-  async list(_user: any) {
-    return this.labs.list();
+  list(user: RequestUser) {
+    return this.labs.list(user);
   }
 
-  async getOne(id: string, _user: any) {
-    return this.labs.getById(id);
+  getOne(id: string, user: RequestUser) {
+    return this.labs.getById(id, user);
   }
 
-  create(dto: CreateSampleDto, user: any) {
+  create(dto: CreateSampleDto, user: RequestUser) {
     return this.labs.create(dto, user);
   }
 
-  collect(id: string, user: any) {
-    return this.labs.collect(id, user);
+  collect(id: string, dto: CollectSampleDto, user: RequestUser) {
+    return this.labs.collect(id, dto, user);
   }
 
-  recordTest(id: string, dto: RecordTestDto, user: any) {
+  recordTest(id: string, dto: RecordTestDto, user: RequestUser) {
     return this.labs.recordTest(id, dto, user);
   }
 
-  finalize(id: string, user: any) {
-    return this.labs.finalize(id, user);
+  finalize(id: string, dto: FinalizeSampleDto, user: RequestUser) {
+    return this.labs.finalize(id, dto, user);
   }
 }
