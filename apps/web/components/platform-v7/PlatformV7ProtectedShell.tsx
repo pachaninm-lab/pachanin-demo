a22cf8522c523d9ecda4b85e3c3eeeea3cf01864
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 19:53:33 +0300

    Switch protected business routes to Transaction App Shell v8

--- apps/web/components/platform-v7/PlatformV7ProtectedShell.tsx
+++ apps/web/components/platform-v7/PlatformV7ProtectedShell.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { AppShellV4 } from '@/components/v7r/AppShellV4';
+import { TransactionAppShell } from '@/components/transaction-ux/TransactionAppShell';
 import { ShellCopyNormalizer } from '@/components/v7r/ShellCopyNormalizer';
 import { ScopedShellGuard } from '@/components/platform-v7/ScopedShellGuard';
 import { RbacCabinetGuard } from '@/components/platform-v7/RbacCabinetGuard';
@@ -191,7 +191,7 @@ export function PlatformV7ProtectedShell({ pathname, children }: { pathname: str
   return (
     <>
       <ShellCopyNormalizer />
-      <AppShellV4 initialRole={initialRole}>
+      <TransactionAppShell initialRole={initialRole}>
         <>
           <ScopedShellGuard />
           <PlatformV7SingleEntryGuard />
@@ -209,7 +209,7 @@ export function PlatformV7ProtectedShell({ pathname, children }: { pathname: str
           {showPlatformFooter ? <PlatformFooter /> : null}
           <OnboardingTour />
         </>
-      </AppShellV4>
+      </TransactionAppShell>
     </>
   );
 }
