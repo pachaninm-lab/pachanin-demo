196d3b4a8853ce0aafaf913a3a024becb8f0b3a8
pachaninm-lab <pachaninm@gmail.com>
2026-07-13 10:28:53 +0300

    feat(labs): require durable sample creation command

--- apps/api/src/modules/labs/dto/create-sample.dto.ts
+++ apps/api/src/modules/labs/dto/create-sample.dto.ts
@@ -1,14 +1,33 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsISO8601, IsOptional, IsString, Length } from 'class-validator';
 
 export class CreateSampleDto {
   @IsString()
+  @Length(1, 200)
   dealId!: string;
 
   @IsOptional()
   @IsString()
+  @Length(1, 200)
   shipmentId?: string;
 
   @IsOptional()
   @IsString()
+  @Length(1, 1000)
   note?: string;
+
+  @IsString()
+  @Length(1, 200)
+  commandId!: string;
+
+  @IsString()
+  @Length(1, 200)
+  idempotencyKey!: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 200)
+  correlationId?: string;
+
+  @IsISO8601({ strict: true })
+  expectedDealUpdatedAt!: string;
 }
