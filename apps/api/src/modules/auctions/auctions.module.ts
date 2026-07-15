2afcb5cb75b18ab14bc167b0c242f16924855e8c
pachaninm-lab <pachaninm@gmail.com>
2026-07-15 10:03:32 +0300

    IR-AUCTION: atomic bids, single winner and restart recovery

--- apps/api/src/modules/auctions/auctions.module.ts
+++ apps/api/src/modules/auctions/auctions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuctionAuthorityService } from './auction-authority.service';
+import { AuctionCommandService } from './auction-command.service';
 import { AuctionsController } from './auctions.controller';
 
 @Module({
   controllers: [AuctionsController],
-  providers: [AuctionAuthorityService],
-  exports: [AuctionAuthorityService],
+  providers: [AuctionAuthorityService, AuctionCommandService],
+  exports: [AuctionAuthorityService, AuctionCommandService],
 })
 export class AuctionsModule {}
