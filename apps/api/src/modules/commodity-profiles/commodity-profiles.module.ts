ca70240ec8ed7260d72cbe86eac03830c3ef9d7a
pachaninm-lab <pachaninm@gmail.com>
2026-07-21 09:48:57 +0300

    PC-CROP-01B.3: add private commodity profile authority module

---
+++ apps/api/src/modules/commodity-profiles/commodity-profiles.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../common/prisma/prisma.module';
+import { CommodityProfileRepository } from './commodity-profile.repository';
+import {
+  COMMODITY_PROFILE_TRANSACTION_PORT,
+  CommodityProfileTransactionCommandService,
+} from './commodity-profile-transaction-command.service';
+import { PostgresqlCommodityProfileTransactionPort } from './postgresql-commodity-profile-transaction.port';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [
+    CommodityProfileRepository,
+    PostgresqlCommodityProfileTransactionPort,
+    {
+      provide: COMMODITY_PROFILE_TRANSACTION_PORT,
+      useExisting: PostgresqlCommodityProfileTransactionPort,
+    },
+    CommodityProfileTransactionCommandService,
+  ],
+  exports: [
+    CommodityProfileRepository,
+    CommodityProfileTransactionCommandService,
+  ],
+})
+export class CommodityProfilesModule {}
