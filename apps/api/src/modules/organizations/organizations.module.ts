3874abfaa25ac24b31a2fbb06ddc027434b4654e
pachaninm-lab <pachaninm@gmail.com>
2026-07-23 14:26:50 +0300

    refactor(intake): keep organizations module unchanged

--- apps/api/src/modules/organizations/organizations.module.ts
+++ apps/api/src/modules/organizations/organizations.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
-import { OrganizationIntakeModule } from '../organization-intake/organization-intake.module';
 
 @Module({
-  imports: [OrganizationIntakeModule],
   providers: [OrganizationsService],
   controllers: [OrganizationsController],
   exports: [OrganizationsService],
