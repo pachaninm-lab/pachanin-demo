bf3a1610ea9ceff0203f02b580347534fb819f92
pachaninm-lab <pachaninm@gmail.com>
2026-07-23 14:25:56 +0300

    feat(intake): mount intake through organizations context

--- apps/api/src/modules/organizations/organizations.module.ts
+++ apps/api/src/modules/organizations/organizations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
+import { OrganizationIntakeModule } from '../organization-intake/organization-intake.module';
 
 @Module({
+  imports: [OrganizationIntakeModule],
   providers: [OrganizationsService],
   controllers: [OrganizationsController],
   exports: [OrganizationsService],
