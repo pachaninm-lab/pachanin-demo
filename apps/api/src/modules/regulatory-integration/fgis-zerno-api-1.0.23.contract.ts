f2006b2ee29b3e7cc4948a604cbd75e6c1e7612d
pachaninm-lab <pachaninm@gmail.com>
2026-07-24 14:23:11 +0300

    PC-CROP-08B: add typed operation unions and runtime validation

---
+++ apps/api/src/modules/regulatory-integration/fgis-zerno-api-1.0.23.contract.ts
@@ -0,0 +1,100 @@
+/* Generated from the hash-pinned PC-CROP-08B catalog. Do not hand-edit. */
+export const FGIS_ZERNO_API_VERSION = '1.0.23' as const;
+export const FGIS_ZERNO_PACKAGE_SHA256 = '085e22c50b6564219585c96e814b0793d906f4c5e401cbb7446a949c26f0bcd7' as const;
+export const FGIS_ZERNO_CONTRACT_CATALOG_SHA256 = 'db9332b124d71a097620e36f52568adafcb42ff3637fc7ad1a77afd76a125bf1' as const;
+export const FGIS_ZERNO_TRANSPORT_OPERATIONS = ['Ack','SendRequest','SendResponse'] as const;
+export const FGIS_ZERNO_BUSINESS_FAMILIES = ['DICTIONARIES','GPB','GPB_SDIZ','GRAIN_MONITOR','LOTS','RSHN_DOCUMENTS','SDIZ','VED_CONTRACT'] as const;
+export const FGIS_ZERNO_OPERATION_CATALOG = [
+  ["DICTIONARIES","DICTIONARIES_GET_DICTIONARY","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/dictionaries/1.0.23}Request","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/dictionaries/1.0.23}Response"],
+  ["GPB","GPB_CANCELED_GPB","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestCanceledGpb","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseCanceledGpb"],
+  ["GPB","GPB_CANCELED_GPB_DEBIT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestCanceledGpbDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseCanceledGpbDebit"],
+  ["GPB","GPB_CREATE_GPB","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestCreateGpb","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseCreateGpb"],
+  ["GPB","GPB_CREATE_GPB_DEBIT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestCreateGpbDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseCreateGpbDebit"],
+  ["GPB","GPB_GET_LIST_GPB","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestGetListGpb","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseGetListGpb"],
+  ["GPB","GPB_GET_LIST_GPB_DEBIT","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}RequestGetListGpbDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb/1.0.23}ResponseGetListGpbDebit"],
+  ["GPB_SDIZ","GPB_SDIZ_CANCELED_GPB_EXTINCTION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCanceledGpbExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCanceledGpbExtinction"],
+  ["GPB_SDIZ","GPB_SDIZ_CANCELED_GPB_EXTINCTION_REFUSAL","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCanceledGpbExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCanceledGpbExtinctionRefusal"],
+  ["GPB_SDIZ","GPB_SDIZ_CANCELED_GPB_SDIZ","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCanceledGpbSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCanceledGpbSDIZ"],
+  ["GPB_SDIZ","GPB_SDIZ_CREATE_GPB_EXTINCTION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCreateGpbExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCreateGpbExtinction"],
+  ["GPB_SDIZ","GPB_SDIZ_CREATE_GPB_EXTINCTION_REFUSAL","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCreateGpbExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCreateGpbExtinctionRefusal"],
+  ["GPB_SDIZ","GPB_SDIZ_CREATE_GPB_SDIZ","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestCreateGpbSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseCreateGpbSDIZ"],
+  ["GPB_SDIZ","GPB_SDIZ_GET_LIST_GPB_EXTINCTION","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestGetListGpbExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseGetListGpbExtinction"],
+  ["GPB_SDIZ","GPB_SDIZ_GET_LIST_GPB_EXTINCTION_REFUSAL","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestGetListGpbExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseGetListGpbExtinctionRefusal"],
+  ["GPB_SDIZ","GPB_SDIZ_GET_LIST_GPB_SDIZ","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}RequestGetListGpbSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/gpb-sdiz/1.0.23}ResponseGetListGpbSDIZ"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CANCELED_GM_APPLICATION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCanceledGmApplication","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCanceledGmApplication"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CANCELED_HARVESTED_CROP","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCanceledHarvestedCrop","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCanceledHarvestedCrop"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CANCELED_PRIMARY_STORAGE_PLACE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCanceledPrimaryStoragePlace","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCanceledPrimaryStoragePlace"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CREATE_GM_APPLICATION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCreateGmApplication","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCreateGmApplication"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CREATE_HARVESTED_CROP","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCreateHarvestedCrop","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCreateHarvestedCrop"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_CREATE_PRIMARY_STORAGE_PLACE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestCreatePrimaryStoragePlace","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseCreatePrimaryStoragePlace"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_DELETE_PRIMARY_STORAGE_PLACE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestDeletePrimaryStoragePlace","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseDeletePrimaryStoragePlace"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_GET_LIST_GM_APPLICATION","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestGetListGmApplication","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseGetListGmApplication"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_GET_LIST_HARVESTED_CROP","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestGetListHarvestedCrop","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseGetListHarvestedCrop"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_GET_LIST_PRIMARY_STORAGE_PLACE","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestGetListPrimaryStoragePlace","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseGetListPrimaryStoragePlace"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_GET_LIST_RESEARCH","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestGetListResearch","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseGetListResearch"],
+  ["GRAIN_MONITOR","GRAIN_MONITOR_GET_LIST_SAMPLES_PICKING","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}RequestGetListSamplesPicking","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/grain-monitor/1.0.23}ResponseGetListSamplesPicking"],
+  ["LOTS","LOTS_CANCELED_LOT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCanceledLot","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCanceledLot"],
+  ["LOTS","LOTS_CANCELED_LOT_DEBIT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCanceledLotDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCanceledLotDebit"],
+  ["LOTS","LOTS_CANCEL_PURCHASE_FROM_INDIVIDUAL_DOC","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCancelPurchaseFromIndividualDoc","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCancelPurchaseFromIndividualDoc"],
+  ["LOTS","LOTS_CREATE_LOT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCreateLot","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCreateLot"],
+  ["LOTS","LOTS_CREATE_LOT_DEBIT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCreateLotDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCreateLotDebit"],
+  ["LOTS","LOTS_CREATE_LOT_ON_ELEVATOR","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCreateLotOnElevator","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCreateLotOnElevator"],
+  ["LOTS","LOTS_CREATE_PURCHASE_FROM_INDIVIDUAL_DOC","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestCreatePurchaseFromIndividualDoc","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseCreatePurchaseFromIndividualDoc"],
+  ["LOTS","LOTS_GET_LIST_LOT","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestGetListLot","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseGetListLot"],
+  ["LOTS","LOTS_GET_LIST_LOT_DEBIT","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestGetListLotDebit","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseGetListLotDebit"],
+  ["LOTS","LOTS_GET_LIST_PURCHASE_FROM_INDIVIDUAL_DOC","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}RequestGetListPurchaseFromIndividualDoc","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/lots/1.0.23}ResponseGetListPurchaseFromIndividualDoc"],
+  ["RSHN_DOCUMENTS","RSHN_DOCUMENTS_CREATE_CERTIFICATE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}RequestCreateCertificate","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}ResponseCreateCertificate"],
+  ["RSHN_DOCUMENTS","RSHN_DOCUMENTS_CREATE_EXPORT_VET_CERTIFICATE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}RequestCreateExportVetCertificate","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}ResponseCreateExportVetCertificate"],
+  ["RSHN_DOCUMENTS","RSHN_DOCUMENTS_CREATE_VET_CERTIFICATE","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}RequestCreateVetCertificate","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}ResponseCreateVetCertificate"],
+  ["RSHN_DOCUMENTS","RSHN_DOCUMENTS_CREATE_ZKFS","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}RequestCreateZKFS","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/rshn-documents/1.0.23}ResponseCreateZKFS"],
+  ["SDIZ","SDIZ_CANCELED_EXTINCTION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCanceledExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCanceledExtinction"],
+  ["SDIZ","SDIZ_CANCELED_EXTINCTION_REFUSAL","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCanceledExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCanceledExtinctionRefusal"],
+  ["SDIZ","SDIZ_CANCELED_SDIZ","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCanceledSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCanceledSDIZ"],
+  ["SDIZ","SDIZ_CREATE_EXTINCTION","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCreateExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCreateExtinction"],
+  ["SDIZ","SDIZ_CREATE_EXTINCTION_REFUSAL","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCreateExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCreateExtinctionRefusal"],
+  ["SDIZ","SDIZ_CREATE_SDIZ","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCreateSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCreateSDIZ"],
+  ["SDIZ","SDIZ_CREATE_SDIZ_ELEVATOR","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestCreateSDIZElevator","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseCreateSDIZElevator"],
+  ["SDIZ","SDIZ_GET_LIST_EXTINCTION","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestGetListExtinction","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseGetListExtinction"],
+  ["SDIZ","SDIZ_GET_LIST_EXTINCTION_REFUSAL","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestGetListExtinctionRefusal","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseGetListExtinctionRefusal"],
+  ["SDIZ","SDIZ_GET_LIST_SDIZ","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestGetListSDIZ","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseGetListSDIZ"],
+  ["SDIZ","SDIZ_GET_LIST_SDIZ_ELEVATOR","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}RequestGetListSDIZElevator","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/sdiz/1.0.23}ResponseGetListSDIZElevator"],
+  ["VED_CONTRACT","VED_CONTRACT_CANCEL_VED_CONTRACT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}RequestCancelVedContract","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}ResponseCancelVedContract"],
+  ["VED_CONTRACT","VED_CONTRACT_CLOSE_VED_CONTRACT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}RequestCloseVedContract","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}ResponseCloseVedContract"],
+  ["VED_CONTRACT","VED_CONTRACT_CREATE_VED_CONTRACT","MUTATION","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}RequestCreateVedContract","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}ResponseCreateVedContract"],
+  ["VED_CONTRACT","VED_CONTRACT_GET_LIST_VED_CONTRACT","READ","SendRequest","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}RequestGetListVedContract","{urn://x-artefacts-mcx-gov-ru/fgiz-zerno/api/ws/ved-contract/1.0.23}ResponseGetListVedContract"],
+] as const;
+export type FgisZernoTransportOperation = typeof FGIS_ZERNO_TRANSPORT_OPERATIONS[number];
+export type FgisZernoBusinessFamily = typeof FGIS_ZERNO_BUSINESS_FAMILIES[number];
+export type FgisZernoOperationRow = typeof FGIS_ZERNO_OPERATION_CATALOG[number];
+export type FgisZernoBusinessOperationCode = FgisZernoOperationRow[1];
+export type FgisZernoOperationClassification = FgisZernoOperationRow[2];
+export type FgisZernoSignatureMetadataReference = Readonly<{ algorithm:string; keyReference:string; keyVersion:string; policyVersion:string }> | null;
+export type FgisZernoEnvelopeMetadata = Readonly<{ messageId:string; correlationId:string; schemaVersion:typeof FGIS_ZERNO_API_VERSION; mappingVersion:typeof FGIS_ZERNO_API_VERSION; contentSha256:string; signatureMetadataReference:FgisZernoSignatureMetadataReference }>;
+export type FgisZernoOperationSelection = Readonly<{ adapterCode:'FGIS_ZERNO'; apiVersion:typeof FGIS_ZERNO_API_VERSION; family:FgisZernoBusinessFamily; operationCode:FgisZernoBusinessOperationCode; transportOperation:FgisZernoTransportOperation; requestQName:string; responseQName:string; envelope:FgisZernoEnvelopeMetadata }>;
+export const FGIS_ZERNO_GATEWAY_CONTRACT = Object.freeze({ adapterCode:'FGIS_ZERNO',apiVersion:FGIS_ZERNO_API_VERSION,packageSha256:FGIS_ZERNO_PACKAGE_SHA256,catalogSha256:FGIS_ZERNO_CONTRACT_CATALOG_SHA256,status:'ADAPTER_READY',operationalStatus:'NOT_ATTESTED',confirmedLive:false,capabilities:Object.freeze(['SCHEMA_MAPPING','CONTRACT_CATALOG'] as const),runtimeEndpoint:null,credentialReference:null,certificateReference:null } as const);
+export class FgisZernoContractValidationError extends Error { constructor(readonly code:string){ super(code); this.name='FgisZernoContractValidationError'; } }
+const OPERATION_INDEX=new Map(FGIS_ZERNO_OPERATION_CATALOG.map((row)=>[row[1],row] as const));
+const SHA256=/^[0-9a-f]{64}$/;
+function nonEmpty(value:unknown):value is string{return typeof value==='string'&&value.trim().length>0;}
+export function assertFgisZernoOperationSelection(value:FgisZernoOperationSelection):FgisZernoOperationRow{
+ if(value.adapterCode!=='FGIS_ZERNO')throw new FgisZernoContractValidationError('ADAPTER_CODE_INVALID');
+ if(value.apiVersion!==FGIS_ZERNO_API_VERSION)throw new FgisZernoContractValidationError('API_VERSION_INVALID');
+ const operation=OPERATION_INDEX.get(value.operationCode); if(!operation)throw new FgisZernoContractValidationError('OPERATION_UNKNOWN');
+ if(operation[0]!==value.family)throw new FgisZernoContractValidationError('FAMILY_MISMATCH');
+ if(operation[3]!==value.transportOperation)throw new FgisZernoContractValidationError('TRANSPORT_OPERATION_MISMATCH');
+ if(operation[4]!==value.requestQName)throw new FgisZernoContractValidationError('REQUEST_QNAME_MISMATCH');
+ if(operation[5]!==value.responseQName)throw new FgisZernoContractValidationError('RESPONSE_QNAME_MISMATCH');
+ const envelope=value.envelope;
+ if(!nonEmpty(envelope.messageId)||!nonEmpty(envelope.correlationId))throw new FgisZernoContractValidationError('MESSAGE_IDENTITY_INVALID');
+ if(envelope.schemaVersion!==FGIS_ZERNO_API_VERSION||envelope.mappingVersion!==FGIS_ZERNO_API_VERSION)throw new FgisZernoContractValidationError('ENVELOPE_VERSION_INVALID');
+ if(!SHA256.test(envelope.contentSha256))throw new FgisZernoContractValidationError('CONTENT_HASH_INVALID');
+ const signature=envelope.signatureMetadataReference;
+ if(signature!==null&&(!nonEmpty(signature.algorithm)||!nonEmpty(signature.keyReference)||!nonEmpty(signature.keyVersion)||!nonEmpty(signature.policyVersion)))throw new FgisZernoContractValidationError('SIGNATURE_METADATA_REFERENCE_INVALID');
+ return operation;
+}
+export function assertFgisZernoRuntimeEndpoint(value:string):string{
+ let parsed:URL; try{parsed=new URL(value);}catch{throw new FgisZernoContractValidationError('ENDPOINT_INVALID');}
+ const host=parsed.hostname.toLowerCase();
+ if(parsed.protocol!=='https:'||host==='localhost'||host==='127.0.0.1'||host==='::1')throw new FgisZernoContractValidationError('PLACEHOLDER_OR_INSECURE_ENDPOINT_FORBIDDEN');
+ return parsed.toString();
+}
