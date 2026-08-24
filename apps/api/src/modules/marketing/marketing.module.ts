ddbd1edb4080df6fe45b67bb8755527f62a06d9a
pachaninm-lab <pachaninm@gmail.com>
2026-08-24 12:32:51 +0300

    feat(marketing): register VK marketing connector (#4616)

--- apps/api/src/modules/marketing/marketing.module.ts
+++ apps/api/src/modules/marketing/marketing.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MarketingPolicyService } from './marketing-policy.service';
 import { MarketingPublisherService } from './marketing-publisher.service';
 import { TelegramPublisher } from './connectors/telegram.publisher';
+import { VkPublisher } from './connectors/vk.publisher';
 
 @Module({
-  providers: [MarketingPolicyService, MarketingPublisherService, TelegramPublisher],
+  providers: [MarketingPolicyService, MarketingPublisherService, TelegramPublisher, VkPublisher],
   exports: [MarketingPolicyService, MarketingPublisherService],
 })
 export class MarketingModule {}
